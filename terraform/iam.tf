1586861a7edbd56f777c7bd0d17d7f0ce54c18c1
mickoscode <mick.oconnor@gmail.com>
2025-08-09 18:56:25 +0800

    add user for github workflow

---
+++ terraform/iam.tf
@@ -0,0 +1,27 @@
+resource "aws_iam_user" "mickos_github" {
+  name = "mickos-github"
+}
+
+resource "aws_iam_policy" "mickos_github_s3_write_policy" {
+  name        = "mickos-github-s3-write-policy"
+  description = "Policy to allow write access to the S3 bucket mickos-surf-club-website"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowWriteToBucket"
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:PutObjectAcl"
+        ]
+        Resource = "arn:aws:s3:::mickos-surf-club-website/*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_user_policy_attachment" "mickos_github_policy_attachment" {
+  user       = aws_iam_user.mickos_github.name
+  policy_arn = aws_iam_policy.mickos_github_s3_write_policy.arn
+}

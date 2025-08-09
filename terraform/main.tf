8ea851c946d742d9a0458044a6147716be3e6fd0
mickoscode <mick.oconnor@gmail.com>
2025-08-09 15:01:27 +0800

    add terraform dir

---
+++ terraform/main.tf
@@ -0,0 +1,24 @@
+terraform { 
+  cloud { 
+    
+    organization = "mickoscode" 
+
+    workspaces { 
+      name = "mc-dev-cli" 
+    } 
+  } 
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-southeast-2"
+}

1cc81b6fc07823821a6792c85d6fb4598884e55f
mickoscode <mick.oconnor@gmail.com>
2025-08-12 20:39:08 +0800

    fix api - must always be POST for AWS_PROXY

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -42,7 +42,7 @@ resource "aws_apigatewayv2_integration" "fetch_integration" {
   api_id                 = aws_apigatewayv2_api.api.id
   integration_type       = "AWS_PROXY"
   integration_uri        = aws_lambda_function.fetch_logs.invoke_arn
-  integration_method     = "GET"
+  integration_method     = "POST"
   payload_format_version = "2.0"
 }
 

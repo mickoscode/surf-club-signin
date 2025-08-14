1cd016c8ef8155cb8682731a22e6c411211a36a7
mickoscode <mick.oconnor@gmail.com>
2025-08-14 20:15:51 +0800

    fmt lambda.tf

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -30,7 +30,7 @@ resource "aws_lambda_function" "fetch_logs" {
   filename         = data.archive_file.fetch_logs_zip.output_path
   source_code_hash = data.archive_file.fetch_logs_zip.output_base64sha256
 }
- 
+
 data "archive_file" "fetch_names_zip" {
   type        = "zip"
   source_dir  = "${path.module}/lambda_fetch_names"

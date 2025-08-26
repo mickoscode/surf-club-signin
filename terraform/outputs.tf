5d3e607cdb5e16957b77ae78b81dc6b5644799f2
mickoscode <mick.oconnor@gmail.com>
2025-08-26 16:56:31 +0800

    fix: delete all waf v1 acl resources

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,6 +8,16 @@ output "acm_validation_record" {
     }
   }
 }
+# Output the DNS validation records for 2nd cert
+output "acm_validation_record2" {
+  value = {
+    for dvo in aws_acm_certificate.domain2.domain_validation_options : dvo.domain_name => {
+      name  = dvo.resource_record_name
+      type  = dvo.resource_record_type
+      value = dvo.resource_record_value
+    }
+  }
+}
 
 # An output value to get the CloudFront domain name, which you will use
 # to create a CNAME record in your DNS.

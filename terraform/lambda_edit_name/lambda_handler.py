10f3fd2ffd361055e4010825b3c884724f20ad48
mickoscode <mick.oconnor@gmail.com>
2025-08-31 15:40:47 +0800

    fix: correct lambda handler and add logging

--- terraform/lambda_edit_name/lambda_handler.py
+++ terraform/lambda_edit_name/lambda_handler.py
@@ -16,10 +16,10 @@ def lambda_handler(event, context):
         name_id = body["name_id"]
         activity_id = body["activity_id"]
         display = body["display"]
-        filter = body["filter"]
+        filter_val = body["filter"]  #filter is a reserved word, hence need for attibute name mapping below!
     
         # Validate inputs
-        if not all([activity_id, name_id, display, filter]):
+        if not all([activity_id, name_id, display, filter_val]):
             return build_response(400, {"message": "Missing required fields."})
 
         print("Attempting update for:", name_id, activity_id)
@@ -30,14 +30,15 @@ def lambda_handler(event, context):
                 "name_id": {"S": name_id},
                 "activity_id": {"S": activity_id}
             },
-            UpdateExpression="SET display = :d, filter = :f",
-            ExpressionAttributeValues={ ":d": {"S": display}, ":f": {"S": filter} },
+            UpdateExpression="SET display = :d, #f = :f",
+            ExpressionAttributeNames={ "#f": "filter" },
+            ExpressionAttributeValues={ ":d": {"S": display}, ":f": {"S": filter_val} },
             ConditionExpression="attribute_exists(name_id) AND attribute_exists(activity_id)"
         )
         #return {"statusCode": 200, "body": json.dumps({"message": "Name updated"})}
         return build_response(201, {"message": "Name updated successfully."})
 
-    except dynamodb.meta.client.exceptions.ConditionalCheckFailedException:
+    except dynamodb.exceptions.ConditionalCheckFailedException:
         print("Conditional check failed for:", name_id, activity_id)
         return build_response(409, {"message": "Could not match an existing entry for updating"})
 

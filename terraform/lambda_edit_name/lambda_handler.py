1f47219b10a62f52fdf0c4951a844f180eed3be1
mickoscode <mick.oconnor@gmail.com>
2025-08-31 10:57:49 +0800

    fix: add error handling to edit name lambda

--- terraform/lambda_edit_name/lambda_handler.py
+++ terraform/lambda_edit_name/lambda_handler.py
@@ -7,27 +7,37 @@ def lambda_handler(event, context):
     if method == "OPTIONS":
         return build_response(200, {"message": "CORS preflight OK"})
 
-    body = json.loads(event["body"])
-    name_id = body["name_id"]
-    activity_id = body["activity_id"]
-    display = body["display"]
-    filter_val = body["filter"]
+    try:
+        # parse inputs
+        body = json.loads(event["body"])
+        name_id = body["name_id"]
+        activity_id = body["activity_id"]
+        display = body["display"]
+        filter = body["filter"]
+    
+        # Validate inputs
+        if not all([activity_id, name_id, display, filter]):
+            return build_response(400, {"message": "Missing required fields."})
 
-    dynamodb = boto3.client("dynamodb")
-    dynamodb.update_item(
-        TableName="names",
-        Key={
-            "name_id": {"S": name_id},
-            "activity_id": {"S": activity_id}
-        },
-        UpdateExpression="SET display = :d, filter = :f",
-        ExpressionAttributeValues={
-            ":d": {"S": display},
-            ":f": {"S": filter_val}
-        }
-    )
-    #return {"statusCode": 200, "body": json.dumps({"message": "Name updated"})}
-    return build_response(201, {"message": "Name updated successfully."})
+        dynamodb = boto3.client("dynamodb")
+        dynamodb.update_item(
+            TableName="names",
+            Key={
+                "name_id": {"S": name_id},
+                "activity_id": {"S": activity_id}
+            },
+            UpdateExpression="SET display = :d, filter = :f",
+            ExpressionAttributeValues={ ":d": {"S": display}, ":f": {"S": filter} },
+            ConditionExpression="attribute_exists(name_id) AND attribute_exists(activity_id)"
+        )
+        #return {"statusCode": 200, "body": json.dumps({"message": "Name updated"})}
+        return build_response(201, {"message": "Name updated successfully."})
+
+    except dynamodb.meta.client.exceptions.ConditionalCheckFailedException:
+        return build_response(409, {"message": "Could not match an existing entry for updating"})
+
+    except Exception as e:
+        return build_response(500, {"message": "Internal server error", "error": str(e)})
 
 # Add cors headers to the response
 def build_response(status_code, body):
